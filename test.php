b45c6f4a72ad9136b02412eaa011c45070a5e996
qaz12312 <watermoo22@gmail.com>
2020-11-15 20:37:59 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,4 @@
+<?php
 $num = 3; 
 $name = "匿名";
 $total = $num + 10*2;
@@ -8,4 +9,51 @@ echo $name."不會顯示username";
 
 if($nume == "匿名"){
     echo "you cannot see username";
+}elseif($num != 3){
+    echo "you don't have Manager Authority";
 }
+?>
+<?php
+$arr = array();
+$arr1[0][0] = "aaa";
+$arr1[0][1] = "bbb";
+$arr1[1][0] = "ccc";
+$arr1[1][1] = "ddd";
+foreach($arr1 as $val){
+    foreach($val as $val2){
+        echo "$val2";
+    }
+}
+$b= NUll;
+$c = 0; 
+$d = 1;
+$e = ""; 
+$g = "Abc";
+foreach($arr as $idx => $val){
+    if(isset($a)){//變數是不是有存在
+        //變數存在，且值為NULL，則 FALSE
+        //變數存在，且值為0、1、空字串、不為NULL，則TRUE 
+
+    }
+}
+?>
+<?php
+?>
+<?php
+?>
+<?php
+?>
+<?php
+?>
+<?php
+?>
+<?php
+?>
+<?php
+?>
+<?php
+?>
+<?php
+?>
+<?php
+?>
